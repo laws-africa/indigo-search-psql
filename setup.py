5830b2774b163f88619d1013068dfe2f19182a5e
Greg Kempe <greg@kempe.net>
2020-08-13 16:45:52 +0200

    Packaging

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+
+setup(
+    name='indigo-search-psql',
+    version=version,
+    description='Indigo platform plugin for PSQL-based free-text search',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/laws-africa/indigo-search-psql',
+
+    # Author details
+    author='Laws.Africa',
+    author_email='greg@laws.africa',
+
+    # See https://pypi.org/classifiers/
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Legal Industry',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    packages=find_packages(exclude=['docs']),
+    include_package_data=True,
+
+    python_requires='~=3.6',
+    install_requires=[
+        'django>=2.2.12,<3',
+        'indigo>=3',
+    ],
+)
